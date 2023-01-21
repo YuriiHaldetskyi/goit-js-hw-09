32574ae6567b31a8656bda57b6f3ef88d05c977b
YuriiHaldetskyi <110350105+YuriiHaldetskyi@users.noreply.github.com>
2023-01-21 23:10:49 +0000

    Deploying to gh-pages from @ YuriiHaldetskyi/goit-js-hw-09@a703b410de9c29f7b14e76dea474deda5c7b9d35 🚀

---
+++ 01-color-switcher.12b1f751.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;e.disabled=!0,t.addEventListener("click",(function(n){e.disabled=!1,t.disabled=!0,d=setInterval((t=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(n){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.12b1f751.js.map
--- 01-color-switcher.905bb6ed.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;e.disabled=!0,t.addEventListener("click",(function(n){e.disabled=!1,t.disabled=!0,d=setInterval((t=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(d){clearInterval(timeId),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.905bb6ed.js.map
